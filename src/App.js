c31ba33951f4e68e97bafcb7b724517592e908b5
nik frank <nikbfrank@gmail.com>
2019-07-23 06:31:15 -0700

    built-chess

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Piece from 'react-chess-pieces';
+
+const initialPosition = [
+  ["R", "N", "B", "Q", "K", "B", "N", "R"],
+  ["P", "P", "P", "P", "P", "P", "P", "P"],
+  ["", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", ""],
+  ["p", "p", "p", "p", "p", "p", "p", "p"],
+  ["r", "n", "b", "q", "k", "b", "n", "r"]
+];
+
+
+class App extends React.Component {
+  state = {
+    pieces: initialPosition,
+
+  }
+
+  clickCell = (row, col)=>{
+    const selectedPiece = (this.state.pieces[this.state.selectedRow] || []
+                          )[this.state.selectedCol];
+
+    if( selectedPiece ){
+      let nextPieces = this.state.pieces;
+
+      nextPieces[this.state.selectedRow][this.state.selectedCol] = '';
+      nextPieces[row][col] = selectedPiece;
+      
+      this.setState({
+        pieces: nextPieces,
+
+        selectedRow: null,
+        selectedCol: null,
+      });
+
+    } else {
+      if( this.state.pieces[row][col] )
+        this.setState({
+          selectedRow: row,
+          selectedCol: col,
+        });
+    }
+  }
+
+  render() {
+    const { pieces, selectedCol, selectedRow } = this.state;
+
+    return (
+      <div className="App">
+        <div className='board'>
+          {[0, 1, 2, 3, 4, 5, 6, 7].map(row => (
+            <div className='row' key={row}>
+              {[0, 1, 2, 3, 4, 5, 6, 7].map(col => (
+                <div className={((selectedRow === row) && (selectedCol === col)) ?
+                                 'cell selected' : 'cell'}
+                     key={col}
+                     onClick={()=> this.clickCell(row, col)}>
+                  <Piece piece={pieces[row][col]}/>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
